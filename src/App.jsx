764c8b96f23dcf5e28657c92422d44d75b81e94b
Roman Hrynko <romanhrynkoua@gmail.com>
2021-07-22 22:32:22 +0300

    add-store

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import TodoList from '../src/tasks/components/TodoList.jsx';
+import { Provider } from 'react-redux';
+import store from './store.js';
 
 const App = () => {
-  return <TodoList />;
+  return (
+    <Provider store={store}>
+      <TodoList />
+    </Provider>
+  );
 };
 
 export default App;
