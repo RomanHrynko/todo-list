26d463700afeda494228ccd71db610b3ff97d3fd
Roman Hrynko <romanhrynkoua@gmail.com>
2021-07-23 16:45:21 +0300

    todo-list-final

--- src/tasks/components/TasksList.jsx
+++ src/tasks/components/TasksList.jsx
@@ -1,68 +1,38 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import Task from './Task.jsx';
 import CreateTaskInput from './CreateTaskInput.jsx';
-import { createTask, updateTask, deleteTask } from '../tasksGateway.js';
 import { connect } from 'react-redux';
 import * as tasksActions from '../tasks.actions';
-import { tasksListSelector } from '../tasks.selectors';
+import { sortedTasksListSelector } from '../tasks.selectors';
 
-class TasksList extends Component {
-  componentDidMount() {
-    this.props.getTasksList();
-  }
+const TasksList = ({ tasks, getTasksList, createTask, updateTask, deleteTask }) => {
+  useEffect(() => {
+    getTasksList();
+  }, []);
 
-  onCreate = text => {
-    const newTask = {
-      text,
-      done: false,
-    };
-
-    createTask(newTask).then(() => this.props.getTasksList());
-  };
-
-  handleTaskStatusChange = id => {
-    const { done, text } = this.props.tasks.find(task => task.id === id);
-    const updatedTask = {
-      text,
-      done: !done,
-    };
-
-    updateTask(id, updatedTask).then(() => this.props.getTasksList());
-  };
-
-  handleTaskDelete = id => {
-    deleteTask(id).then(() => this.props.getTasksList());
-  };
-
-  render() {
-    const sortedList = this.props.tasks.slice().sort((a, b) => a.done - b.done);
-
-    return (
-      <div className="todo-list">
-        <CreateTaskInput onCreate={this.onCreate} />
-        <ul className="list">
-          {sortedList.map(task => (
-            <Task
-              key={task.id}
-              {...task}
-              onChange={this.handleTaskStatusChange}
-              onDelete={this.handleTaskDelete}
-            />
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="todo-list">
+      <CreateTaskInput onCreate={createTask} />
+      <ul className="list">
+        {tasks.map(task => (
+          <Task key={task.id} {...task} onChange={updateTask} onDelete={deleteTask} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 const mapState = state => {
   return {
-    tasks: tasksListSelector(state),
+    tasks: sortedTasksListSelector(state),
   };
 };
 
 const mapDispatch = {
   getTasksList: tasksActions.getTasksList,
+  updateTask: tasksActions.updateTask,
+  deleteTask: tasksActions.deleteTask,
+  createTask: tasksActions.createTask,
 };
 
 export default connect(mapState, mapDispatch)(TasksList);
